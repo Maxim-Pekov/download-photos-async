576521714084a71ee24769261ce0844c40a1cdc9
Maxim-Pekov <max.pekov@gmail.com>
2023-08-05 16:15:06 +0300

    Add argparse module.

--- server.py
+++ server.py
@@ -2,43 +2,49 @@ import os
 import logging
 import asyncio
 import aiofiles
+import argparse
 
 from aiohttp import web
 
+
+DOWNLOAD_DELAY = 0
 SIZE = 100_000
 logger = logging.getLogger(__name__)
 
 
 async def archive(request):
     hash = request.match_info.get('archive_hash', "Anonymous")
-    files_path = os.path.join('test_photos', hash)
+    files_path = os.path.join(app.args.photo_folder, hash)
     response = web.StreamResponse()
     if not os.path.exists(files_path):
         logger.info(f'There are no photos in the directory "{files_path}"')
-        return web.HTTPNotFound(reason=404, text='Ошибка 404, папка с фотографиями удалена')
+        return web.HTTPNotFound(
+            reason=404,
+            text='Ошибка 404, папка с фотографиями удалена')
     proc = await asyncio.create_subprocess_exec(
         "zip", "-r", "-", ".",
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE,
         cwd=files_path)
     response.headers['Content-Type'] = 'text/html'
-    response.headers['Content-Disposition'] = 'attachment; filename="photos.zip"'
+    response.headers['Content-Disposition'] = ('attachment; '
+                                               'filename="photos.zip"')
     await response.prepare(request)
     chunk_count = 1
     try:
         while not proc.stdout.at_eof():
-            x = await proc.stdout.read(SIZE)
-            await response.write(x)
+            archive_chunk = await proc.stdout.read(SIZE)
+            await response.write(archive_chunk)
             logger.info(f'Sending archive chunk №{chunk_count} ({SIZE} bytes)')
             chunk_count += 1
-            raise SystemExit
-            await asyncio.sleep(1)
+            await asyncio.sleep(DOWNLOAD_DELAY)
     except asyncio.CancelledError:
-        logger.info(f'Download was interrupted')
+        logger.info('Download was interrupted')
         raise
     finally:
-        proc.kill()
-        proc.communicate()
+        if not proc.stdout.at_eof():
+            proc.kill()
+            proc.communicate()
     return response
 
 
@@ -49,10 +55,35 @@ async def handle_index_page(request):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        datefmt='%d-%m-%Y %I:%M:%S %p',
-                        level=logging.INFO)
+    def create_parser():
+        parser = argparse.ArgumentParser(
+            description='скачивает фотографии одним архивом ')
+        parser.add_argument(
+            '-l', '--is_logging',
+            action='store_true',
+            help='Что бы включить логирование, укажите этот параметр')
+        parser.add_argument(
+            '-d', '--download_delay',
+            action='store_true',
+            help='Укажите флаг, чтобы включить задержку скачивания в 1 сек.')
+        parser.add_argument(
+            '-f', '--photo_folder',
+            help='путь к каталогу с фотографиями',
+            default='test_photos', type=str, nargs='?')
+        return parser
+    parser = create_parser()
+
     app = web.Application()
+    app.args = parser.parse_args()
+
+    if app.args.is_logging:
+        logging.basicConfig(
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            datefmt='%d-%m-%Y %I:%M:%S %p',
+            level=logging.INFO)
+    if app.args.download_delay:
+        DOWNLOAD_DELAY = 1
+
     app.add_routes([
         web.get('/', handle_index_page),
         web.get('/archive/{archive_hash}', archive),
